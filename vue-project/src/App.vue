876b2612ca2b83593426505acad97d88772dfe14
Wang55566 <wang6@hawaii.edu>
2024-02-20 13:57:36 -0800

    replace options api with composition api

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,20 +8,10 @@
   </div>
 </template>
 
-<script>
+<script setup>
  import Header from './components/Header.vue';
  import Balence from './components/Balence.vue';
  import IncomeExpenses from './components/IncomeExpenses.vue';
  import TransactionList from './components/TransactionList.vue';
  import AddTransaction from './components/AddTransaction.vue';
-
- export default {
-  components: {
-    Header,
-    Balence,
-    IncomeExpenses,
-    TransactionList,
-    AddTransaction
-  }
- }
 </script>
--- vue-project/src/components/TransactionList.vue
+++ vue-project/src/components/TransactionList.vue
@@ -1,13 +1,19 @@
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
-    <li class="minus">
-      Cash <span>-$400</span>
-      <button class="delete-btn">x</button>
-    </li>
-    <li class="plus">
-      Paycheck <span>$800</span>
+    <li v-for="transaction in transactions"
+        :key='transaction.id'
+        :class="transaction.amount < 0? 'minus': 'plus'"
+    >
+      {{transaction.text}}<span>${{transaction.amount}}</span>
       <button class="delete-btn">x</button>
     </li>
   </ul>
 </template>
+
+<script setup>
+    const transactions = [
+      { id:1, text: 'Flower', amount: 20.00 },
+      { id:2, text: 'Speaker', amount: -30.00 },
+    ];
+</script>
