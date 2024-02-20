fadcc42643c0b02abfeca403ceb784add3b951d4
Wang55566 <wang6@hawaii.edu>
2024-02-19 23:41:29 -0800

    finish layout

---
+++ vue-project/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <Header />
+  <div class="container">
+    <Balence />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<script>
+ import Header from './components/Header.vue';
+ import Balence from './components/Balence.vue';
+ import IncomeExpenses from './components/IncomeExpenses.vue';
+ import TransactionList from './components/TransactionList.vue';
+ import AddTransaction from './components/AddTransaction.vue';
+
+ export default {
+  components: {
+    Header,
+    Balence,
+    IncomeExpenses,
+    TransactionList,
+    AddTransaction
+  }
+ }
+</script>
---
+++ vue-project/src/components/Balence.vue
@@ -0,0 +1,4 @@
+<template>
+  <h4>Your Balence</h4>
+  <h1 id="balence">$0.00</h1>
+</template>
